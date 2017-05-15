1906c23bc570788e82698a6ef2d01578a84ba110
Julia Yi <juliamijiyi@gmail.com>
2017-05-15 10:02:07 -0700

    Add method-override & change app.js to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
 
 app.use('/', index);
 app.use('/users', users);
