206ae6b7d754eb87594a0c0dee5dce44636e03f4
Sam Skelton <samuellskelton@gmail.com>
2017-05-17 15:24:51 -0700

    makes map less ugly

--- controllers/trips.js
+++ controllers/trips.js
@@ -53,7 +53,6 @@ var tripController = {
     Trip.findById(req.params.id, (err, trip) => {
       var tripjson = trip.toObject();
       tripjson = JSON.stringify(tripjson)
-      console.log(location)
       console.log(tripjson)
       console.log('typeof', typeof tripjson)
       res.render('show', {trip: trip, user: req.user, tripJSON: tripjson});
