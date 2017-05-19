bdf621561ada9cfc9f19056a7bc1f7739fb2eb8b
Julia Yi <juliamijiyi@gmail.com>
2017-05-19 10:08:35 -0700

    Clean up code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -62,8 +62,6 @@ function searchTrips() {
   filter = input.value.toLowerCase();
   tripCards = document.querySelectorAll('.tripio-card');
   tripCards.forEach(function(card) {
-    console.log('searchsearchsearch')
     card.style.display = (card.attributes.getNamedItem('data-tags').value.includes(filter)) ? '' : 'none';
   });
 };
-
